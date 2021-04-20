a9e29c37990069ea2c23c4fb39c3d51f24b4d208
mihimadui <k.peeranut@gmail.com>
2021-04-20 10:38:11 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- META -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="#">
+    <!-- PERFORMANCE -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap">
+    <link rel="preload" as="style" href="style.css">
+    <link rel="stylesheet" href="style.css">
+    <!-- TITLE -->
+    <title>IP Address Blacklist</title>
+</head>
+<body>
+<header role="banner" class="ui-section-header">
+    <div class="ui-layout-container">
+        <div class="ui-section-header__layout ui-layout-flex">
+            <!-- LOGO -->
+            <a href="#" role="link" aria-label="#">
+<!--                <svg viewBox="0 0 18 18" height="18" width="18" role="img" aria-label="#">-->
+<!--                    <path fill="#353535" d="M0 0h4.5v9a4.5 4.5 0 109 0V6H18v3A9 9 0 110 9V0zm18 4.5V0h-4.5v4.5H18z"/>-->
+<!--                </svg>-->
+                NHSO
+            </a>
+            <!-- MENU -->
+            <a href="https://www.google.com/search?q=IP+Address+Blacklist+Check" role="link" aria-label="#"
+               class="ui-component-button ui-component-button-small ui-component-button-primary" target="_blank">Check IP Address</a>
+        </div>
+    </div>
+</header>
+<main role="main">
+
+    <section class="ui-section-close">
+        <div class="ui-layout-container">
+            <div class="ui-layout-column-6 ui-layout-column-center">
+                <h2>ไม่สามารถเข้าใช้งานเว็บไซต์ได้เนื่องจาก</h2>
+                <!-- CTA -->
+                <div class="ui-component-cta ui-layout-flex">
+                    <a href="#" role="link" aria-label="#" style="font-size: 28px;"
+                       class="ui-component-button ui-component-button-normal ui-component-button-primary">IP Address
+                        <?=$_SERVER['REMOTE_ADDR']?></a>
+<!--                    <p class="ui-text-note"><small>Firefox and Safari soon.</small></p>-->
+                </div>
+                <h3>ของท่านไม่ปลอดภัย (IP Address Blacklist)</h3>
+
+                <p>กรุณาติดต่อเจ้าหน้าที่ IT หรือ ติดต่อกับผู้ให้บริการอินเตอร์เน็ตของหน่วยงานท่านเพื่อทำการตรวจสอบ IP
+                    Address Blacklist</p>
+                <p>ท่านสามารถตรวจสอบ IP Address ที่ไม่ปลอดภัยได้จากเว็บไซต์ตรวจสอบ IP Address Blacklist
+                    โดยสามารถตรวจสอบได้ที่ <a href="https://www.google.com/search?q=IP+Address+Blacklist+Check"><b>Check IP Address</b></a></p>
+<!--                www.google.com พิมพ์ IP Address Blacklist Check-->
+                <br/>
+            </div>
+            <!-- IMAGE -->
+<!--            <img src="https://res.cloudinary.com/uisual/image/upload/assets/devices/ipad-t.png" loading="lazy" alt="#"-->
+<!--                 class="ui-section-hero--image">-->
+        </div>
+    </section>
+    <section class="ui-section-feature">
+        <div class="ui-layout-container">
+            <div class="ui-section-feature__layout ui-layout-grid ui-layout-grid-2">
+                <!-- IMAGE -->
+                <img src="90449.png" loading="lazy" alt="#" class="ui-image-half-left">
+                <!-- FEATURE -->
+                <div>
+                    <h2>คำแนะนำเบื้องต้น</h2>
+                    <ul class="ui-component-list ui-component-list-feature ui-layout-column-center">
+                        <li class="ui-component-list--item ui-component-list--item-check">1. ทำการตรวจสอบ IP Address Blacklist</li>
+                        <li class="ui-component-list--item ui-component-list--item-check">2. ตรวจสอบสาเหตุการ Blacklist จากเว็บไซต์ตรวจสอบ IP Address Blacklist</li>
+                        <li class="ui-component-list--item ui-component-list--item-check">3. ตรวจหาต้นเหตุที่ทำให้เกิด IP Address Blacklist โดยสืบค้นข้อมูลจากอุปกรณ์เครือข่ายของหน่วยงานท่าน</li>
+                        <li class="ui-component-list--item ui-component-list--item-check">4. กำจัด หรือหยุด หรือแยก ต้นเหตุนั้นออกจากระบบเครือข่ายของท่าน</li>
+                        <li class="ui-component-list--item ui-component-list--item-check">5. ดำเนินการแจ้งข้อมูลให้กับเว็บไซต์ตรวจสอบ IP Address Blacklist นั้นๆ เพื่อออกจากฐานข้อมูล Blacklist</li>
+                    </ul>
+                    <p class="">หากมีข้อซักถามสามารถติดต่อมาที่ <b>NetworkAdmin@nhso.go.th</b><br/>ขออภัยในความไม่สะดวกมา ณ ที่นี้</p>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+<footer role="contentinfo" class="ui-section-footer">
+    <div class="ui-layout-container">
+        <div class="ui-section-footer__layout ui-layout-flex">
+            <!-- COPYRIGHT -->
+            <p class="ui-section-footer--copyright ui-text-note"><small>&copy; NHSO</small></p>
+            <!-- MENU -->
+<!--            <nav role="navigation" class="ui-section-footer--nav ui-section-footer--menu ui-layout-flex">-->
+<!--                <a href="#" role="link" aria-label="#" class="ui-text-note"><small>Features</small></a>-->
+<!--                <a href="#" role="link" aria-label="#" class="ui-text-note"><small>Help</small></a>-->
+<!--                <a href="#" role="link" aria-label="#" class="ui-text-note"><small>Terms</small></a>-->
+<!--            </nav>-->
+            <!-- SOCIAL -->
+<!--            <nav role="navigation" class="ui-section-footer--nav ui-section-footer--social ui-layout-flex">-->
+<!--                <a href="#" role="link" aria-label="#">-->
+<!--                    <svg viewBox="0 0 24 24" height="16" width="16" fill="none" stroke="#AEAEAE" stroke-linecap="round"-->
+<!--                         stroke-linejoin="round" stroke-width="2" role="img" aria-label="#">-->
+<!--                        <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"/>-->
+<!--                    </svg>-->
+<!--                </a>-->
+<!--                <a href="#" role="link" aria-label="#">-->
+<!--                    <svg viewBox="0 0 24 24" height="16" width="16" fill="none" stroke="#AEAEAE" stroke-linecap="round"-->
+<!--                         stroke-linejoin="round" stroke-width="2" role="img" aria-label="#">-->
+<!--                        <rect width="20" height="20" x="2" y="2" rx="5" ry="5"/>-->
+<!--                        <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zM17.5 6.5h.01"/>-->
+<!--                    </svg>-->
+<!--                </a>-->
+<!--                <a href="#" role="link" aria-label="#">-->
+<!--                    <svg viewBox="0 0 24 24" height="16" width="16" fill="none" stroke="#AEAEAE" stroke-linecap="round"-->
+<!--                         stroke-linejoin="round" stroke-width="2" role="img" aria-label="#">-->
+<!--                        <circle cx="12" cy="12" r="10"/>-->
+<!--                        <path d="M8.56 2.75c4.37 6.03 6.02 9.42 8.03 17.72m2.54-15.38c-3.72 4.35-8.94 5.66-16.88 5.85m19.5 1.9c-3.5-.93-6.63-.82-8.94 0-2.58.92-5.01 2.86-7.44 6.32"/>-->
+<!--                    </svg>-->
+<!--                </a>-->
+<!--            </nav>-->
+        </div>
+    </div>
+</footer>
+</body>
+</html>
